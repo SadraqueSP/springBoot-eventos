9704a8131d50baa35f85e68784c9e536dae3921a
Sadraque <sadraque.live@hotmail.com>
2018-05-20 21:09:01 -0300

    Agora é possível salvar e visualizar os eventos

--- src/main/java/br/com/ssp/eventos/controller/EventosController.java
+++ src/main/java/br/com/ssp/eventos/controller/EventosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.ssp.eventos.model.EventoModel;
 import br.com.ssp.eventos.repository.EventoRepository;
@@ -28,5 +29,16 @@ public class EventosController {
 		
 		return "redirect:/cadastrarEvento";
 	}
+	
+	@RequestMapping("/")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		
+		Iterable<EventoModel> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		
+		return mv;
+	}
+	
 	
 }
